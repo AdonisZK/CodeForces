3d88eb50a3d7d15418e8ac556f93797603d18664
AdonisZK <adonisvefleyor@gmail.com>
2022-11-09 23:30:31 +0700

    Update : < 1300

--- 1000/Supercentral_Point.cpp
+++ 1000/Supercentral_Point.cpp
@@ -4,13 +4,49 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int query, x, y, ans = 0;
+    int query, x[205], y[205], i, j, ans = 0, xmin, xplus, ymin, yplus, count = 0;
     cin >> query;
 
-    for (int i = 0; i < query; i++)
+    for (i = 0; i < query; i++)
     {
-        cin >> x >> y;
+        cin >> x[i] >> y[i];
     }
+
+    for (i = 0; i < query; i++)
+    {
+        xmin = xplus = ymin = yplus = 0;
+        for (j = 0; j < query; j++)
+        {
+            if (i == j)
+            {
+                continue;
+            }
+            if (x[i] < x[j] && y[i] == y[j])
+            {
+                xmin = 1;
+            }
+            else if (x[i] > x[j] && y[i] == y[j])
+            {
+                xplus = 1;
+            }
+            else if (x[i] == x[j] && y[i] < y[j])
+            {
+                ymin = 1;
+            }
+            else if (x[i] == x[j] && y[i] > y[j])
+            {
+                yplus = 1;
+            }
+            if (xmin + xplus + ymin + yplus == 4)
+            {
+                break;
+            }
+        }
+        if (xmin + xplus + ymin + yplus == 4)
+        {
+            count++;
+        }
+    }
+    cout << count << endl;
     return 0;
 }
-
--- tes.cpp
+++ tes.cpp
@@ -3,24 +3,9 @@ using namespace std;
 
 int main()
 {
+    int a, b, c, d;
 
-    // Get the vector
-    vector<int> a = {1, 45, 54, 71, 76, 12};
+    a = b = c = d = 5;
 
-    // Print the vector
-    cout << "Vector: ";
-    for (int i = 0; i < a.size(); i++)
-        cout << a[i] << " ";
-    cout << endl;
-
-    // Reverse the vector
-    reverse(a.begin(), a.end());
-
-    // Print the reversed vector
-    cout << "Reversed Vector: ";
-    for (int i = 0; i < a.size(); i++)
-        cout << a[i] << " ";
-    cout << endl;
-
-    return 0;
+    cout << a << b << c << d << endl;
 }
