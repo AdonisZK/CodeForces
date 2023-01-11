25fda0e1163af0b333493f494c9da1ce6f13fc42
Mikask1 <darrenprasetya40@gmail.com>
2023-01-11 16:06:05 +0700

    Update : < 1300

--- tes.cpp
+++ tes.cpp
@@ -1,9 +1,10 @@
+#define MOD 1000000007
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int a = 5, ans = 3;
-    cout << setprecision(2) << 1.2523 * ans << endl;
+
     return 0;
 }
